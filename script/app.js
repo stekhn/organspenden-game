5bcd5af754542e26c236a6a65d52c2b0fd0b7cac
Steffen Kühne <stekhn@gmail.com>
2014-10-25 15:17:41 +0200

    Add HTML markup

--- script/app.js
+++ script/app.js
@@ -2,5 +2,14 @@
 
 	var app = angular.module('app', []);
 
+	app.controller('JsonLoaderCtrl', ['$scope', '$http', function ($scope, $http) {
+
+		$http.get('data/data.json').success(function (data) {
+
+			$scope.data = data;
+		});
+
+	}]);
+
 	
 }());
