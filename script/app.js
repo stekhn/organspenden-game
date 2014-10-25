485ed0654ad5a1f420ecb380b79205159487fe21
Steffen Kühne <stekhn@gmail.com>
2014-10-25 20:21:27 +0200

    Add render controller, directive

--- script/app.js
+++ script/app.js
@@ -14,6 +14,28 @@
 
     }]);
 
+    app.directive('onFinishRender', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit(attr.onFinishRender);
+                    });
+                }
+            }
+        };
+    });
+
+
+    app.controller('PatientController', ['$scope', function ($scope) {
+
+        $scope.$on('done', function(ngRepeatFinishedEvent) {
+
+            console.log("Patients rendered.");
+        });
+    }]);
+
     function generatePatients($scope) {
 
         $scope.patients = [];
